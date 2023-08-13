d52d03c68aae351931e6cd91f1fd644adc03489e
sjoseph92 <sjoseph92@gmail.com>
2023-08-13 18:29:26 -0400

    chore: change page name

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,4 +1,4 @@
-export default function Example() {
+export default function Page() {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-white">
