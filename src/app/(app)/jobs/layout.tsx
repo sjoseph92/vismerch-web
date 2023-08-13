8b258958cc0e77be6e06f93e2ee1e633276ba5a1
sjoseph92 <sjoseph92@gmail.com>
2023-08-13 18:35:16 -0400

    feat: make bg white

--- src/app/(app)/jobs/layout.tsx
+++ src/app/(app)/jobs/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function JobsLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="py-10 lg:pl-72">
+    <main className="bg-white h-screen py-10 lg:pl-72">
       <div className="px-4 sm:px-6 lg:px-8">{children}</div>
     </main>
   );
