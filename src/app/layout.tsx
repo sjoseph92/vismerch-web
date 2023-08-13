4760317e497c054315a4241c7107b23056ce3b77
sjoseph92 <sjoseph92@gmail.com>
2023-08-13 18:27:23 -0400

    chore: remove css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html className="h-full bg-white" lang="en">
-      <body className={`h-full ${inter.className}`}>{children}</body>
+    <html lang="en">
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
