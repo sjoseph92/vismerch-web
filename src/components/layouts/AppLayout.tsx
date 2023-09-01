c713bdfd6bfe1081d8e32be976e698a4b8e7c61a
sjoseph92 <sjoseph92@gmail.com>
2023-09-01 17:11:38 -0400

    chore: add mobile header comp

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 
 import { navigation } from "@/consts";
 import SidebarLink from "./SidebarLink";
+import MobileHeader from "./MobileHeader";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -129,28 +130,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
             </nav>
           </div>
         </div>
-
-        <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
-            onClick={() => setSidebarOpen(true)}
-          >
-            <span className="sr-only">Open sidebar</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-          <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">
-            Dashboard
-          </div>
-          <a href="#">
-            <span className="sr-only">Your profile</span>
-            <img
-              className="h-8 w-8 rounded-full bg-gray-50"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
-          </a>
-        </div>
+        <MobileHeader setSidebarOpen={setSidebarOpen} />
         {children}
       </div>
     </>
---
+++ src/components/layouts/MobileHeader.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Dispatch, FC, SetStateAction } from "react";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+
+interface MobileHeaderProps {
+  setSidebarOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const MobileHeader: FC<MobileHeaderProps> = ({ setSidebarOpen }) => {
+  return (
+    <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+      <button
+        type="button"
+        className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+        onClick={() => setSidebarOpen(true)}
+      >
+        <span className="sr-only">Open sidebar</span>
+        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+      </button>
+      <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">
+        Dashboard
+      </div>
+      <a href="#">
+        <span className="sr-only">Your profile</span>
+        <Image
+          className="h-8 w-8 rounded-full bg-gray-50"
+          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt=""
+          height={8}
+          width={8}
+        />
+      </a>
+    </div>
+  );
+};
+
+export default MobileHeader;
