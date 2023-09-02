cb2102871449efd4699ef574d4bcec324ca2d33f
sjoseph92 <sjoseph92@gmail.com>
2023-09-02 08:54:33 -0400

    chore: add mock products bucket

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "tailwindui.com"],
+    domains: [
+      "images.unsplash.com",
+      "tailwindui.com",
+      "mockproducts.s3.amazonaws.com",
+    ],
   },
   async redirects() {
     return [
