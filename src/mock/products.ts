a7d6ac4b203a5ef5eac95350fd53d50bc118c939
sjoseph92 <sjoseph92@gmail.com>
2023-09-04 16:47:52 -0400

    chore: add mock product

---
+++ src/mock/products.ts
@@ -0,0 +1,58 @@
+import cherryLimeadePoppiPack from "@/public/cherry-limeade-poppi-pack.webp";
+import docPopPoppiCan from "@/public/doc-pop-poppi-can.webp";
+import orangePoppiCan from "@/public/orange-poppi-can.webp";
+import rasberryRosePoppiCan from "@/public/rasberry-rose-poppi-can.webp";
+import rasberryRosePoppiPack from "@/public/rasberry-rose-poppi-pack.webp";
+import strawberryLemonadePoppiCan from "@/public/strawberry-lemonade-poppi-can.webp";
+
+
+export const products = [
+  {
+    id: 1,
+    name: "Poppi Raspberry Rose Prebiotic Soda - 12 fl oz Can",
+    image: rasberryRosePoppiCan,
+    gtin: "709586514894",
+    type: "GTIN-12",
+    basePrice: "$2.49",
+  },
+  {
+    id: 2,
+    name: "Poppi Strawberry Lemon Prebiotic Soda - 12 fl oz Can",
+    image: strawberryLemonadePoppiCan,
+    gtin: "709586514856",
+    type: "GTIN-12",
+    basePrice: "$2.49",
+  },
+  {
+    id: 3,
+    name: "Poppi Doc Pop Prebiotic Soda - 12 fl oz Can",
+    image: docPopPoppiCan,
+    gtin: "810063710309",
+    type: "GTIN-12",
+    basePrice: "$2.49",
+  },
+  {
+    id: 4,
+    name: "Poppi Orange Prebiotic Soda - 12 fl oz Can",
+    image: orangePoppiCan,
+    gtin: "724190812877",
+    type: "GTIN-12",
+    basePrice: "$2.49",
+  },
+  {
+    id: 5,
+    name: "Poppi Raspberry Rose Prebiotic Soda - 4pk/12 fl oz Cans",
+    image: rasberryRosePoppiPack,
+    gtin: "810063710064",
+    type: "GTIN-12",
+    basePrice: "$8.99",
+  },
+  {
+    id: 6,
+    name: "Poppi Cherry Limeade Prebiotic Beverage - 4pk/12 fl oz Cans",
+    image: cherryLimeadePoppiPack,
+    gtin: "810063710279",
+    type: "GTIN-12",
+    basePrice: "$8.99",
+  },
+];
