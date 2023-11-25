a78edb725588864ccf371c1bf57dffc4f7383eb7
sjoseph92 <sjoseph92@gmail.com>
2023-11-25 10:43:20 -0500

    chore: update style, replace a tag with next link

--- src/components/layouts/LocationDetailsLayout/Breadcrumbs.tsx
+++ src/components/layouts/LocationDetailsLayout/Breadcrumbs.tsx
@@ -1,37 +1,30 @@
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
-
-const pages = [
-  { name: "Projects", href: "#", current: false },
-  { name: "Project Nero", href: "#", current: true },
-];
+import Link from "next/link";
 
 const Breadcrumbs = () => {
   return (
     <nav className="flex" aria-label="Breadcrumb">
       <ol role="list" className="flex items-center space-x-1 lg:space-x-2">
-        <li>
-          <a
+        <li className="flex items-center">
+          <Link
             href="/locations"
             className="text-sm font-medium text-gray-500 hover:text-gray-700"
             aria-current="page"
           >
             Locations
-          </a>
+          </Link>
         </li>
-        <li>
-          <ChevronRightIcon
-            className="h-5 w-5 flex-shrink-0 text-gray-400"
-            aria-hidden="true"
-          />
+        <li className="flex items-center">
+          <ChevronRightIcon className="h-5 w-5 flex-shrink-0 text-gray-400" />
         </li>
-        <li>
-          <a
+        <li className="flex items-center">
+          <Link
             href="/locations"
-            className="text-sm font-medium text-gray-500 hover:text-gray-700"
+            className="text-sm font-medium text-gray-500 hover:text-gray-700 text-center"
             aria-current="page"
           >
             Walgreens #15442
-          </a>
+          </Link>
         </li>
       </ol>
     </nav>
